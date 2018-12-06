8b86f4b6da003c0fddb92e27637bd581ee1a9b1f
rushikc <1ms16cs021@msrit.edu>
2018-12-06 10:33:47 +0530

    linux

--- 3.cc
+++ 3.cc
@@ -1,19 +1,3 @@
-/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
-/*
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation;
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- */
-
 #include "ns3/core-module.h"
 #include "ns3/point-to-point-module.h"
 #include "ns3/network-module.h"
@@ -22,6 +6,11 @@
 #include "ns3/mobility-module.h"
 #include "ns3/csma-module.h"
 #include "ns3/internet-module.h"
+
+
+#include "ns3/flow-monitor-module.h"
+
+
 
 // Default Network Topology
 //
@@ -45,17 +34,14 @@ NS_LOG_COMPONENT_DEFINE ("ThirdScriptExample");
 int 
 main (int argc, char *argv[])
 {
-  bool verbose = true;
   uint32_t nCsma = 3;
   uint32_t nWifi = 3;
-  bool tracing = false;
+
+  double simulationTime = 10; //seconds
+
+  std::string socketType = "ns3::UdpSocketFactory";
 
   CommandLine cmd;
-  cmd.AddValue ("nCsma", "Number of \"extra\" CSMA nodes/devices", nCsma);
-  cmd.AddValue ("nWifi", "Number of wifi STA devices", nWifi);
-  cmd.AddValue ("verbose", "Tell echo applications to log if true", verbose);
-  cmd.AddValue ("tracing", "Enable pcap tracing", tracing);
-
   cmd.Parse (argc,argv);
 
   // Check for valid number of csma or wifi nodes
@@ -65,12 +51,6 @@ main (int argc, char *argv[])
     {
       std::cout << "Too many wifi or csma nodes, no more than 250 each." << std::endl;
       return 1;
-    }
-
-  if (verbose)
-    {
-      LogComponentEnable ("UdpEchoClientApplication", LOG_LEVEL_INFO);
-      LogComponentEnable ("UdpEchoServerApplication", LOG_LEVEL_INFO);
     }
 
   NodeContainer p2pNodes;
@@ -93,6 +73,8 @@ main (int argc, char *argv[])
 
   NetDeviceContainer csmaDevices;
   csmaDevices = csma.Install (csmaNodes);
+
+
 
   NodeContainer wifiStaNodes;
   wifiStaNodes.Create (nWifi);
@@ -156,7 +138,7 @@ main (int argc, char *argv[])
   address.Assign (staDevices);
   address.Assign (apDevices);
 
-  UdpEchoServerHelper echoServer (9);
+  /*UdpEchoServerHelper echoServer (9);
 
   ApplicationContainer serverApps = echoServer.Install (csmaNodes.Get (nCsma));
   serverApps.Start (Seconds (1.0));
@@ -170,20 +152,54 @@ main (int argc, char *argv[])
   ApplicationContainer clientApps = 
     echoClient.Install (wifiStaNodes.Get (nWifi - 1));
   clientApps.Start (Seconds (2.0));
-  clientApps.Stop (Seconds (10.0));
+  clientApps.Stop (Seconds (10.0));*/
+
+  // Udp Flow
+  uint16_t port = 7;
+  Address localAddress (InetSocketAddress (Ipv4Address::GetAny (), port));
+  PacketSinkHelper packetSinkHelper (socketType, localAddress);
+  ApplicationContainer sinkApp = packetSinkHelper.Install (csmaNodes.Get (nCsma));
+  // should be modified
+
+  sinkApp.Start (Seconds (0.0));
+  sinkApp.Stop (Seconds (simulationTime + 0.1));
+
+  uint32_t payloadSize = 1448;
+
+  OnOffHelper onoff (socketType, Ipv4Address::GetAny ());
+  onoff.SetAttribute ("OnTime",  StringValue ("ns3::ConstantRandomVariable[Constant=1]"));
+  onoff.SetAttribute ("OffTime", StringValue ("ns3::ConstantRandomVariable[Constant=0]"));
+  onoff.SetAttribute ("PacketSize", UintegerValue (payloadSize));
+  onoff.SetAttribute ("DataRate", StringValue ("50Mbps")); //bit/s
+  ApplicationContainer apps;
+
+  AddressValue remoteAddress (InetSocketAddress (csmaInterfaces.GetAddress (nCsma), port));
+  onoff.SetAttribute ("Remote", remoteAddress);
+  apps.Add (onoff.Install (wifiStaNodes.Get(nWifi-1)));
+  apps.Start (Seconds (1.0));
+  apps.Stop (Seconds (simulationTime + 0.1));
 
   Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
 
+  FlowMonitorHelper flowmon;
+  Ptr<FlowMonitor> monitor = flowmon.InstallAll();
+
   Simulator::Stop (Seconds (10.0));
-
-  if (tracing == true)
-    {
-      pointToPoint.EnablePcapAll ("third");
-      phy.EnablePcap ("third", apDevices.Get (0));
-      csma.EnablePcap ("third", csmaDevices.Get (0), true);
-    }
-
   Simulator::Run ();
+
+
+  Ptr<Ipv4FlowClassifier> classifier = DynamicCast<Ipv4FlowClassifier> (flowmon.GetClassifier ());
+  std::map<FlowId, FlowMonitor::FlowStats> stats = monitor->GetFlowStats ();
+  std::cout << std::endl << "*** Flow monitor statistics ***" << std::endl;
+
+  for(std::map<FlowId, FlowMonitor::FlowStats>::const_iterator iter = stats.begin (); iter!= stats.end(); ++iter)
+  {
+    Ipv4FlowClassifier::FiveTuple t = classifier->FindFlow (iter->first);
+    NS_LOG_UNCOND("Flow ID: " << iter->first << " Src Addr " <<t.sourceAddress << " Dst Addr " << t.destinationAddress);
+    NS_LOG_UNCOND("Number of packet transmitted: " << iter->second.txPackets);
+  }
+
   Simulator::Destroy ();
   return 0;
 }
+
